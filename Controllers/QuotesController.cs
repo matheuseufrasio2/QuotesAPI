188a4d0bc9dc6e16a42da4c38a0d62c509411dc7
Matheus Eufrásio <matheuseufrasio2@gmail.com>
2020-01-07 21:44:24 -0300

    Comecando a primeira API

---
+++ Controllers/QuotesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuotesApi.Data;
+using QuotesApi.Models;
+
+namespace QuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private QuotesDbContext _quotesDbContext;
+
+        public QuotesController(QuotesDbContext quotesDbContext)
+        {
+            _quotesDbContext = quotesDbContext;
+        }
+
+        // GET: api/Quotes
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return _quotesDbContext.Quotes;
+        }
+
+        // GET: api/Quotes/5
+        [HttpGet("{id}", Name = "Get")]
+        public Quote Get(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            return quote;
+        }
+
+        // POST: api/Quotes
+        [HttpPost]
+        public void Post([FromBody] Quote quote)
+        {
+            _quotesDbContext.Quotes.Add(quote);
+            _quotesDbContext.SaveChanges();
+        }
+
+        // PUT: api/Quotes/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Quote quote)
+        {
+            var entity = _quotesDbContext.Quotes.Find(id);
+            entity.Title = quote.Title;
+            entity.Author = quote.Author;
+            entity.Description = quote.Description;
+            _quotesDbContext.SaveChanges();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            _quotesDbContext.Quotes.Remove(quote);
+            _quotesDbContext.SaveChanges();
+        }
+    }
+}
